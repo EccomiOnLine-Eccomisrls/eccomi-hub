1eff1c724240da9af2c8d07cb984c4e797b2a331
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-02 21:33:34 +0000

    fix: isolate and restore command center V2 styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./executive-home.css";
+import "./command-center-v2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
