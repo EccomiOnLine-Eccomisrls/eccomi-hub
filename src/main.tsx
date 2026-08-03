495bb4ddc91d803f725044d3c086431e8c06c83b
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-03 04:32:12 +0200

    style: import ECCOMI OS lab styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./design-system/tokens.css";
 import "./design-system/components.css";
+import "./os/lab/os-lab.css";
 import "./index.css";
 import "./executive-home.css";
 import "./command-center-v2.css";
