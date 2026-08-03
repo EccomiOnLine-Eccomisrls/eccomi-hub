7bd71c03004763fbc8ba4658e45f0c2a03a2bfd9
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-03 02:01:10 +0000

    feat: introduce ECCOMI OS design system foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./design-system/tokens.css";
+import "./design-system/components.css";
 import "./index.css";
 import "./executive-home.css";
 import "./command-center-v2.css";
