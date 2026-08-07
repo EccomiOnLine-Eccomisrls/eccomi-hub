9fe6cc4c0f7d3e668112e5af722e884ce3070724
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-07 10:34:07 +0200

    fix(preview): install Render preview CORS middleware

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,8 @@
 
 from .auth_guard import install_auth_guard
 from .team_routes import install_team_routes
+from .preview_cors import install_preview_cors
 
 install_auth_guard()
 install_team_routes()
+install_preview_cors()
