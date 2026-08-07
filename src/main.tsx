1ae251aa7b64ab57ab655e3faed43955b8746256
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-08 00:01:25 +0200

    feat(manager): load delegation enforcement in preview

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./manager-shell";
 import "./delegation-controls";
+import "./manager-delegation-enforcement";
 import "./design-system/tokens.css";
 import "./design-system/components.css";
 import "./os/lab/os-lab.css";
