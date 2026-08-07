26e878abf28f3047e8ef81d32863482fb1a4a640
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-07 11:54:54 +0200

    feat(auth): enable manager-only workspace shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./manager-shell";
 import "./design-system/tokens.css";
 import "./design-system/components.css";
 import "./os/lab/os-lab.css";
