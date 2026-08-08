1fb4565f7a5085a16c58cc5227cd10fc214afd6e
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-08 07:17:50 +0200

    fix(team): load legacy governance cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./manager-shell";
 import "./delegation-controls";
 import "./manager-delegation-enforcement";
+import "./remove-legacy-delegations";
 import "./design-system/tokens.css";
 import "./design-system/components.css";
 import "./os/lab/os-lab.css";
